96acddcb2999f3ac686f9916bda46b2f89643113
Peter Sanford <psanford@sanford.io>
2025-08-27 08:41:00 -0700

    json output and make fetching full user optional

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ package main // import "github.com/psanford/github-org-users"
 import (
 	"context"
 	"encoding/csv"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -14,14 +16,25 @@ import (
 	"golang.org/x/oauth2"
 )
 
+var outputFormat = flag.String("format", "csv", "Output format (csv|json)")
+var fetchFullUser = flag.Bool("fetch-full-user", false, "Fetch full user record")
+
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <org-name> <access-token>\n", os.Args[0])
+	flag.Parse()
+
+	accessToken := os.Getenv("GITHUB_API_KEY")
+	if accessToken == "" {
+		fmt.Fprintf(os.Stderr, "Must set GITHUB_API_KEY in environment variable")
+	}
+
+	args := flag.Args()
+
+	if len(args) < 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <org-name>\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	orgName := os.Args[1]
-	accessToken := os.Args[2]
+	orgName := args[0]
 
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
@@ -31,8 +44,16 @@ func main() {
 
 	client := github.NewClient(tc)
 
-	out := csv.NewWriter(os.Stdout)
-	out.Write([]string{"login", "name"})
+	header := []string{"login"}
+	if *fetchFullUser {
+		header = []string{"login", "name"}
+	}
+
+	csvOut := csv.NewWriter(os.Stdout)
+	if *outputFormat == "csv" {
+		csvOut.Write(header)
+	}
+	jsonOut := json.NewEncoder(os.Stdout)
 
 	var opt github.ListMembersOptions
 	for {
@@ -43,12 +64,22 @@ func main() {
 
 		for _, u := range members {
 			// fetch the full user record so we have the user's pretty name
-			u, _, err = client.Users.Get(ctx, u.GetLogin())
-			if err != nil {
-				log.Fatalf("User fetch error: %s", err)
+			if *fetchFullUser {
+				u, _, err = client.Users.Get(ctx, u.GetLogin())
+				if err != nil {
+					log.Fatalf("User fetch error: %s", err)
+				}
 			}
-			out.Write([]string{u.GetLogin(), u.GetName()})
-			out.Flush()
+
+			if *outputFormat == "csv" {
+				if len(header) == 1 {
+					csvOut.Write([]string{u.GetLogin()})
+				} else {
+					csvOut.Write([]string{u.GetLogin(), u.GetName()})
+				}
+			} else {
+				jsonOut.Encode(u)
+			}
 		}
 
 		if resp.NextPage == 0 {
